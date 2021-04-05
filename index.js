69dd8c27b460a6746485e865e72879aef641e2c9
Triston <tristonherbst97@gmail.com>
2021-04-05 14:26:45 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+
+function writeCards(array, string) {
+    let new_array = []
+    for (let i = 0; i < array.length; i++) {
+      new_array.push(`Thank you, ${array[i]}, for the wonderful ${string} gift!`);
+    }
+    return new_array
+} 
+
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num)
+        num --
+    }
+}
